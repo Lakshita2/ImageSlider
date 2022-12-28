9f5f0b124a21b7037b81e904e6b5baa829876aa6
Lakshita2 <lakshitanatani2002@gmail.com>
2022-12-28 14:37:49 +0530

    Files added

---
+++ src/components/Slider/DataSlider.js
@@ -0,0 +1,23 @@
+
+
+const dataSlider = [
+  {
+    id: Math.floor(Math.random * 100)
+    
+  },
+  {
+    id: Math.floor(Math.random * 100)
+  },
+  {
+    id:Math.floor(Math.random * 100)
+  },
+  {
+    id: Math.floor(Math.random * 100)
+  },
+  {
+    id: Math.floor(Math.random * 100)
+    
+  },
+];
+
+export default dataSlider;
